ae6bacfb5d743b941fc5ed11fc2ae2b51221d34c
kaushik <kaushikc005@gmail.com>
2023-06-29 13:13:11 +0530

    Minor corrections

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Cards from "./Cards";
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -13,6 +13,7 @@ const CardsSection = ({
 }) => {
   let filteredData = [];
   let searchedData = [];
+  const [loaded,setLoaded]=useState(false)
   if (filter) {
     filteredData = data?.filter(
       (item) => item.card_type === type || item.owner_id === cardHolder
@@ -28,11 +29,13 @@ const CardsSection = ({
       dataLength={data?.length || 24}
       next={fetchData}
       hasMore={true}
-      loader={<p>Loading...</p>}
+      loader={<p className={`${loaded?'hidden':'block'}`}>Loading...</p>}
       endMessage={<p >No more data to load.</p>}
     >
       <section className="lg:grid grid-cols-3 justify-center 
-      items-center z-0 absolute top-52 lg:right-36">
+      items-center z-0 absolute top-52 lg:right-36"
+      
+      onLoadCapture={()=>setLoaded(true)}>
         {!filter
           ? search
             ? searchedData?.map((item, index) => (
